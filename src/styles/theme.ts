2522bf6fdefea734223e2fedf12d544805d9f7f8
Danilo Gonçalves <danilo.santos@grupoboticario.com.br>
2022-11-06 11:15:19 -0300

    feat: set up chakra theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,26 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      "500": "#47585B",
+      "100": "#DADADA",
+      "50": "#F5F8FA",
+    },
+    yellow: {
+      "500": "#FFBA08",
+    },
+  },
+  fonts: {
+    heading: "Poppins",
+    body: "Poppins",
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "gray.50",
+        color: "gray.500",
+      },
+    },
+  },
+});
