383bb78b5c518a744de870f56b236d0adf1c9abc
Danilo Gonçalves <danilo.santos@grupoboticario.com.br>
2022-11-06 11:40:49 -0300

    feat: create header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import { Flex, Image } from "@chakra-ui/react";
+
+export function Header() {
+  return (
+    <Flex
+      as="header"
+      w="100%"
+      h={["12", "24"]}
+      p={["0.5", "1"]}
+      align="center"
+      justify="center"
+    >
+      <Image
+        src="/images/logo.svg"
+        alt="world trip"
+        w={["20", "100%"]}
+        h={["5", "100%"]}
+        maxW={184}
+        maxH={45}
+      />
+    </Flex>
+  );
+}
